4f8c7098d8fd370d8b862661b5640adbf98a4d4d
David Capolupo <david.m.capolupo@gmail.com>
2017-10-07 15:06:15 +0000

    added game componenet - roll

--- Project1.rb
+++ Project1.rb
@@ -4,7 +4,7 @@
 
 puts "\e[32mWelcome to Lucky 6's: The Game!"
     puts "DISCLAIMER:  PLEASE GAMBLE RESPONSIBLY."
-        puts "\e[1mToday is your lucky day! You're at the State Fair and you find a ten dollar bill on the ground!"
+        puts "\e[1mToday is your lucky day! You're at the State Fair and you find a hundred dollar bill on the ground!"
             puts "You grab the bill and notice that you are standing in front of a dice game booth."
         puts "What will you do?"
     puts "Options: play, pocket"
@@ -13,30 +13,48 @@ option = gets.chomp
 case option
     when "play"
         puts "You sit down at the table.  The game operator quickly explains the rules."
-            puts "You can only bet 10 dollars."
-        puts "You then guess a number from one to six, and roll a single die."
-    puts "If you guess correctly, you win double your bet amount.  Lose, and you gotta pay up!"
+            puts "You can only bet a dollar at a time."
+        puts "You then roll a single die."
+    puts "Roll an even number, and you win two dollars!  Roll an odd number, and you must pay up."
 puts "What will you do?"
+puts "Options: bet, leave"
 
-puts "Options: observe, bet"
 when "pocket"
     puts "You pocket the money, already making plans to spend it elsewhere.  You begin to walk away, but you hear a ruckus coming from the dice booth.  You pause moementarily, considering your options." 
-        else
+         puts "\n What will you do next: play or pocket? \n"         
+else
     puts "You're an outside the box thinker, but Lucky 6's is free.  Buy the premium version (USD $300.00)to unlock additional content."
+    puts "Options: play, pocket"
 end
 
-puts "\n What will you do next: play or pocket? \n" 
 response = gets.chomp
 
 case response
     when "play"
-puts "You sit down at the table.  The game operator quickly explains the rules."
-    puts "You can only bet ten dollars."
-        puts "You then guess a number from one to six, and roll a single die."
-            puts "If you guess correctly, you win double your bet amount.  Lose, and you gotta pay up!"
+        puts "You sit down at the table.  The game operator quickly explains the rules."
+            puts "You can only bet a dollar at a time."
+                puts "You then roll a single die."
+            puts "Roll an even number, and you win two dollars!  Roll an odd number, and you must pay up."
+        puts "What will you do?"
+    puts "Options: bet, leave"    
     when "pocket"
-        puts "You turn your back to the dice booth, and walk away briskly.  You enjoy the rest of your time at the Fair and end up spending your new found cash on some new clothes, or something equally boring."
+        puts "You turn your back to the dice booth, and walk away briskly.  You enjoy the rest of your time at the Fair and end up spending your new found cash on some new clothes, or something equally boring. GAME OVER"
     else 
+        puts "You're an outside the box thinker, but Lucky 6's is free.  Buy the premium version (USD $300.00)to unlock additional content."
+    end
+
+response2 = gets.chomp
+case response2
+    when "bet"
+        puts "You bet a dollar, then roll the die."
+        roll = 1 + rand(6)
+    puts roll
+        if roll.even?
+            puts "Congratulations! You rolled an even number!  You won two dollars!"
+        if roll.odd? 
+            puts "Aww, you rolled an odd number.  You lose a dollar. Better luck next time!"
+        puts "What will you do next?"
+            puts "Options: bet, leave"
 end
-
-
+        end
+end
